0e1f895af87108568a877219d8ff33055cb78595
krnl0xsns1nk <krnl0xsns1nk@gmail.com>
2026-02-05 21:21:25 +0100

    add sitemap.ts file again, i forget ar/fr pages

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,10 +1,14 @@
-import type { MetadataRoute } from 'next'
+import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://7asib.vercel.app',
-      lastModified: new Date(),
-    },
-  ]
+          url: "https://7asib.vercel.app/ar",
+          lastModified: new Date(),
+        },
+    {
+          url: "https://7asib.vercel.app/fr",
+          lastModified: new Date(),
+        },
+  ];
 }
